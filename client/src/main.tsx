151ecccf050687c0d121cd22a4b82a30da03233e
Caio Henrique <caiogrechic50@gmail.com>
2024-06-02 18:11:11 -0300

    Ajustando branchs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,34 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-// createServer({
-//   models: {
-//     tarefas: Model
-//   },
-//   routes() {
-//     this.get('/api/tarefas', () => {
-//       return this.schema.all('tarefas')
-//     })
-
-//     this.post('/api/tarefas', (schema, request) => {
-//       const data = JSON.parse(request.requestBody)
-//       return schema.db.tarefas.insert(data)
-//     })
-
-//     this.put('/api/tarefas', (schema, request) => {
-//       const data = JSON.parse(request.requestBody)
-//       return schema.db.tarefas.update(data.id, data)
-//     })
-
-//     this.delete('/api/tarefas/:id', (schema, request) => {
-//       const id = request.params.id
-//       schema.db.tarefas.remove(id)
-//       return { response: 'Tarefa deletada com sucesso! :D' }
-//     })
-//   }
-// })
-
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
